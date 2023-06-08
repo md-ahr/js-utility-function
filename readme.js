f39ef85cdc2a13ae01ecf260aa0b3222a6d7b34d
Abdul Halim <ahr.web.pro@gmail.com>
2023-06-08 18:16:46 +0600

    Update readme file

--- readme.js
+++ readme.js
@@ -41,7 +41,7 @@ yarn add js-utils
 -   \`flattenedArray(array)\`: Flattens a multi-dimensional array into a one-dimensional array.
 
 \`\`\`javascript
-import { uniqueArray, removeFalsyItems, mergedArray, commonElements, areArraysEqual, shuffledArray, flattenedArray } from "js--utils/array";
+import { uniqueArray, removeFalsyItems, mergedArray, commonElements, \nareArraysEqual, shuffledArray, flattenedArray } from "js--utils/array";
 \`\`\`
 
 \`\`\`javascript
